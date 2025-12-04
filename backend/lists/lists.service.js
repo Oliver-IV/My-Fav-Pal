d78990bb69fa7882565b7a0cf1b66b819b274690
Oliver Inzunza Valle <145882378+Oliver-IV@users.noreply.github.com>
2025-12-03 18:57:22 -0700

    Reapply "Merge branch 'main' of https://github.com/Oliver-IV/My-Fav-Pal"
    This reverts commit e37ab9425a84528631fb364793470eb2c6bed6de.

--- backend/lists/lists.service.js
+++ backend/lists/lists.service.js
@@ -1,4 +1,6 @@
 import List from './entities/list.entity.js';
+import Media from '../media/entities/media.entity.js';
+import mongoose from 'mongoose'; 
 
 export default class ListService {
   async getListsByUserId(userId) {
@@ -9,26 +11,54 @@ export default class ListService {
     return List.find({ ownerId: userId }).sort({ createdAt: -1 });
   }
 
+  async getListById(listId, userId) {
+    const list = await List.findOne({ _id: listId, ownerId: userId })
+      .populate('items.mediaId', 'name type platform poster');
+
+    if (!list) {
+      throw new Error('List not found or user does not have permission.');
+    }
+    return list;
+  }
+
   async createList(listData, userId) {
     const newList = new List({
       ...listData,
-      user: userId, 
+      ownerId: userId,
     });
     return newList.save();
   }
 
   async updateList(listId, updateListData, userId) {
-    const list = await List.findOne({ _id: listId, user: userId });
+    const list = await List.findOne({ _id: listId, ownerId: userId });
     if (!list) {
       throw new Error('List not found or user does not have permission.');
     }
-    Object.assign(list, updateListData);
+
+    list.name = updateListData.name;
+    list.description = updateListData.description;
+    list.visibility = updateListData.visibility;
+
+    if (updateListData.items) {
+      const validItems = updateListData.items.filter(item =>
+        mongoose.Types.ObjectId.isValid(item.mediaId)
+      );
+
+      if (validItems.length !== updateListData.items.length) {
+        throw new Error('Some media IDs are invalid.');
+      }
+
+      list.items = validItems.map(item => ({ mediaId: item.mediaId }));
+    }
+
     await list.save();
+    await list.populate('items.mediaId', 'name type platform poster');
+
     return list;
   }
 
   async deleteList(listId, userId) {
-    const list = await List.findOne({ _id: listId, user: userId });
+    const list = await List.findOne({ _id: listId, ownerId: userId });
     if (!list) {
       throw new Error('List not found or user does not have permission.');
     }
